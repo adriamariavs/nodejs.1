e38a3998d05debc6581a08375d43412774da917d
adriamariavs <125682807+adriamariavs@users.noreply.github.com>
2023-09-20 12:53:18 -0300

    Add files via upload
    1°Passo: Copie o código do outro arquivo que eu postei nesse repositório 
    2°Passo: Abra o terminal e ative o nodejs-env(source ./nodejs-env/bin/activate) 3
    °Passo: Crie um arquivo js(sudo nano index.js) 
    4°Passo: Dentro desse arquivo cole o código que você copiou do outro arquivo 
    5°Passo: Modifique a linha 5, troque o texto que está dentro do parênteses('Quarta-feira, 20 de setembro de 2023, Adria Maria Vieira dos Santos') pelo texto que você escolher(não esqueça de deixar dentro das aspas) 
    6°Passo: Salve o arquivo 
    7°Passo: Abra seu navegador e digite "localhost:8009", o 8009 é a porta que eu coloquei, você pode trocar modificando a linha 6 do arquivo

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  var dataAtual = new Date();
+  var dataFormada = dataAtual.toDateString();
+
+  res.end('Adria Maria Vieira dos Santos/nd Data Atual' + dataFormada);
+}).listen(8009);
