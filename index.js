d82983e0b9e345e3dad91f0586a3896282d5f513
adriamariavs <125682807+adriamariavs@users.noreply.github.com>
2023-09-20 13:23:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  var dataAtual = new Date();
+  var dataFormada = dataAtual.toDateString();
+
+  res.end('Adria Maria Vieira dos Santos Data atual: ' + dataFormada);
+}).listen(8009);
